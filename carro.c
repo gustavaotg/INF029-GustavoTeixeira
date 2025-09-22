f7a8044bae86519f4277ae13d1402f46575d8114
Gustavo TG <118117975+gustavaotg@users.noreply.github.com>
2025-09-22 18:33:47 -0300

    carro.c switches das escolhas

--- carro.c
+++ carro.c
@@ -2,29 +2,49 @@
 
 /* Cria uma struct para carros */
 typedef struct carro {
-  int chassi;
-  int ano;
+	int chassi;
+	int ano;
 } carro;
 
 #define TAM 3
 
 typedef struct carros {
 	carro Lista[TAM];
-	int qtdCarros = 0;
-} carros
+	int qtdCarros;
+} carros;
 
 int main() {
-  
-  int escolha = -1;
-  while(escolha) {
-    printf("")
-    printf("[1] Cadastrar");
-    printf("[2] Listar");
-    printf("[3] Modificar");
-    printf("[4] Excluir");
-    printf("[0] Sair");
-    printf("Digite a escolha");
-	  scanf("%i", &escolha);
+	
+	int escolha = -1;
+	while(escolha) {
+		printf("Cadastramento de Carros\n");
+		printf("[1] Cadastrar\n");
+		printf("[2] Listar\n");
+		printf("[3] Modificar\n");
+		printf("[4] Excluir\n");
+		printf("[0] Sair\n");
+		printf("Digite a escolha:\n");
+		scanf("%i", &escolha);
+		
+		switch (escolha) {
+			case 0:
+				printf("Volte Sempre!\n");
+				break;
+			case 1:
+				printf("Cadastrando...\n")
+				break;
+			case 2:
+				break;
+			case 3:
+				printf("Modificando...\n")
+				break;
+			case 4:
+				printf("Excluindo...\n")
+				break;
+			
+		
+		
+		}
     
   }
   
