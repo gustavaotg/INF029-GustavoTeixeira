8f0734eb23215eff3e1146a2417349983856684f
Gustavo TG <118117975+gustavaotg@users.noreply.github.com>
2025-09-28 13:05:34 -0300

    Fiz todas as funções de carro.c exceto a de excluir
    Tinha esquecido de dar commit aqui no github. Então esse vai ser um commit grande.

--- carro.c
+++ carro.c
@@ -13,10 +13,71 @@ typedef struct carros {
 	int qtdCarros;
 } carros;
 
+
+void esperarUsuario() {
+	printf("Pressione enter para continuar...");
+	getchar();
+	getchar(); // 2 getchar() sao necessarios por causa do \n no buffer apos o scanf
+}
+
+void separador() {
+	for (int i = 0; i < 50; i++)
+		printf("-");
+	printf("\n");
+	return;
+}
+
+int imprimirLista(carros* lista) {
+
+	if (lista->qtdCarros == 0) {
+		printf("Sem carros cadastrados!\n");
+		return 1;
+	}
+
+	for (int i = 0; i < lista->qtdCarros; i++) {
+		printf("Carro [%i]|| Chassi: %i | Ano: %i\n", i + 1, lista->Lista[i].chassi, lista->Lista[i].ano);
+	}
+	return 0;
+}
+
+void cadastrarCarro(carros* lista) {
+	if (lista->qtdCarros >= TAM) {
+		printf("Quantidade maxima de carros!\n");
+		return;
+	}
+	printf("Digite o chassi: ");
+	scanf("%i", &(lista->Lista[lista->qtdCarros].chassi));
+	printf("Digite o ano: ");
+	scanf("%i", &(lista->Lista[lista->qtdCarros].ano));
+	lista->qtdCarros++;
+}
+
+void modificarCarro(carros* lista) {
+	int escolha = -1;
+	imprimirLista(lista);
+	while (escolha < 1 || escolha > lista->qtdCarros) {
+		printf("Digite o numero do carro que deseja modificar: ");
+		scanf("%i", &escolha);
+	}
+	printf("Digite o ano: ");
+	scanf("%i", &(lista->Lista[escolha-1].ano));
+	return;
+}
+
+void excluirCarro(carros* lista) {
+
+
+	return;
+}
+
 int main() {
 	
+	carros lstCarros;
+	lstCarros.qtdCarros = 0;
+
 	int escolha = -1;
 	while(escolha) {
+		separador();
 		printf("Cadastramento de Carros\n");
 		printf("[1] Cadastrar\n");
 		printf("[2] Listar\n");
@@ -25,25 +86,28 @@ int main() {
 		printf("[0] Sair\n");
 		printf("Digite a escolha:\n");
 		scanf("%i", &escolha);
-		
+		separador();
+
 		switch (escolha) {
 			case 0:
 				printf("Volte Sempre!\n");
 				break;
-			case 1:
-				printf("Cadastrando...\n");
+			case 1: // Cadastrar
+				cadastrarCarro(&lstCarros);
+				printf("Cadastrando...[%i]\n", lstCarros.qtdCarros);
 				break;
-			case 2:
+			case 2: // Listar
+				imprimirLista(&lstCarros);
+				esperarUsuario();
 				break;
-			case 3:
+			case 3: // Modificar
+				modificarCarro(&lstCarros);
 				printf("Modificando...\n");
 				break;
-			case 4:
+			case 4: // Excluir
+				excluirCarro(&lstCarros);
 				printf("Excluindo...\n");
 				break;
-			
-		
-		
 		}
     
   }
